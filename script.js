3655ca75122671b627c7eb59124882488aa61e15
Yahya <yahyaabbakar@gmail.com>
2023-01-09 16:01:32 +0200

    added display image functionality

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const imgContainer = document.querySelector('.image-container');
 const loader = document.getElementById('loader');
-let count = 1;
-let APIArray = [];
+let count = 5;
+let photosArray = [];
 const APIKey = 'wa4Vrjo2gTY7xOHP5oK8wvKPabFXKdHmsURdQr5r-Cw';
 
 async function getImages() {
-	const imageURL = `https://api.unsplash.com/photos/random?client_id=${APIKey}&${count}`;
+	const imageURL = `https://api.unsplash.com/photos/random?client_id=${APIKey}&count=${count}`;
 	try {
 		const response = await fetch(imageURL);
-		const data = await response.json();
-		console.log(data);
+		photosArray = await response.json();
+		displayImages();
 	} catch (error) {
-		console.log(error);
 		throw error;
 	}
+}
+
+function displayImages() {
+	photosArray.forEach((photo) => {
+		let item = document.createElement('a');
+		item.setAttribute('href', photo.links.html);
+		item.setAttribute('target', '_blank');
+
+		let img = document.createElement('img');
+		img.setAttribute('src', photo.urls.regular);
+		img.setAttribute('alt', photo.alt_description);
+		img.setAttribute('title', photo.alt_description);
+
+		item.appendChild(img);
+		imgContainer.appendChild(item);
+	});
 }
 
 getImages();
